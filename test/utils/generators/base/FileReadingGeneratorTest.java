8b095393a9d13cb97c5c4216cd43556ca9356151
unknown <nkprasa@gmail.com>
2020-04-16 12:45:55 -0500

    Add javadoc/test for FileReadingGenerator

---
+++ test/utils/generators/base/FileReadingGeneratorTest.java
@@ -0,0 +1,44 @@
+package test.utils.generators.base;
+
+import static org.junit.Assert.assertFalse;
+import static test.Assertions.assertGenerates;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.LinkOption;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import src.utils.generators.Generator;
+import src.utils.generators.base.FileReadingGenerator;
+
+public class FileReadingGeneratorTest {
+
+    private String TEST_FILE = "tmp_FileReadingGeneatorTest.txt";
+
+    @Test
+    public void fileReadingGenerator_givesAllContents() throws IOException {
+        writeToFile(TEST_FILE, Arrays.asList("first", "second third"));
+
+        Generator<String> generator = new FileReadingGenerator(TEST_FILE, null);
+        assertGenerates(generator, "first", "second", "third");
+    }
+
+    @Test
+    public void fileReadingGenerator_withDelimiter_givesAllContents() throws IOException {
+        writeToFile(TEST_FILE, Arrays.asList("first,second,third"));
+
+        Generator<String> generator = new FileReadingGenerator(TEST_FILE, ",|\\p{javaWhitespace}+");
+        assertGenerates(generator, "first", "second", "third");
+    }
+
+    static void writeToFile(String path, List<String> contents) throws IOException {
+        Path file = Paths.get(path);
+        Files.write(file, contents);
+    }
+}
