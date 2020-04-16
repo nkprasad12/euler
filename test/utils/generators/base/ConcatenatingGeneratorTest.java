4ab064e67d21cd78501f41886a579a0c409dcc37
unknown <nkprasa@gmail.com>
2020-04-16 11:26:00 -0500

    Add javadoc/unit tests for ConcatenatingGenerator

---
+++ test/utils/generators/base/ConcatenatingGeneratorTest.java
@@ -0,0 +1,70 @@
+package test.utils.generators.base;
+
+import static org.junit.Assert.assertFalse;
+import static test.Assertions.assertGenerates;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import src.utils.generators.Generator;
+import src.utils.generators.base.ConcatenatingGenerator;
+import src.utils.generators.base.IteratorWrappingGenerator;
+
+public class ConcatenatingGeneratorTest {
+
+    private static final String FIRST = "first";
+    private static final String SECOND = "second";
+    private static final String THIRD = "third";
+    private static final String FOURTH = "fourth";
+    private static final String FIFTH = "fifth";
+
+    private static final List<String> EMPTY =
+        Collections.unmodifiableList(Collections.emptyList());
+    private static final List<String> FIRST_THREE =
+        Collections.unmodifiableList(Arrays.asList(FIRST, SECOND, THIRD));
+    private static final List<String> LAST_TWO =
+        Collections.unmodifiableList(Arrays.asList(FOURTH, FIFTH));
+
+    @Test
+    public void concateningGenerator_emptyGenerators_hasEmptyGenerator() {
+        Generator<String> first = new IteratorWrappingGenerator<>(EMPTY.iterator());
+        Generator<String> second = new IteratorWrappingGenerator<>(EMPTY.iterator());
+
+        Generator<String> generator = new ConcatenatingGenerator<>(first, second);
+
+        assertFalse(generator.hasNext());
+    }
+
+    @Test
+    public void concateningGenerator_emptyThenNonEmpty_hasContents() {
+        Generator<String> first = new IteratorWrappingGenerator<>(FIRST_THREE.iterator());
+        Generator<String> second = new IteratorWrappingGenerator<>(EMPTY.iterator());
+
+        Generator<String> generator = new ConcatenatingGenerator<>(first, second);
+
+        assertGenerates(generator, FIRST, SECOND, THIRD);
+    }
+
+    @Test
+    public void concateningGenerator_nonEmptyThenEmpty_hasContents() {
+        Generator<String> first = new IteratorWrappingGenerator<>(EMPTY.iterator());
+        Generator<String> second = new IteratorWrappingGenerator<>(LAST_TWO.iterator());
+
+        Generator<String> generator = new ConcatenatingGenerator<>(first, second);
+
+        assertGenerates(generator, FOURTH, FIFTH);
+    }
+
+    @Test
+    public void concateningGenerator_nonEmptyThenNonEmpty_hasContents() {
+        Generator<String> first = new IteratorWrappingGenerator<>(FIRST_THREE.iterator());
+        Generator<String> second = new IteratorWrappingGenerator<>(LAST_TWO.iterator());
+
+        Generator<String> generator = new ConcatenatingGenerator<>(first, second);
+
+        assertGenerates(generator, FIRST, SECOND, THIRD, FOURTH, FIFTH);
+    }
+}
