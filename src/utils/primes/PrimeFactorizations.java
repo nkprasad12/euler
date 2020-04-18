d0341a36b16b219a2a89af79799a3545fa51a80f
unknown <nkprasa@gmail.com>
2020-04-18 03:24:26 -0500

    Add unit tests for PrimeFactorizations

--- src/utils/primes/PrimeFactorizations.java
+++ src/utils/primes/PrimeFactorizations.java
@@ -4,6 +4,7 @@ import src.utils.generators.Generator;
 import src.utils.generators.Generators;
 import src.utils.generators.specialized.DivisorGenerator;
 
+/** Factory class for prime factorizations. */
 public final class PrimeFactorizations {
 
   private final Primes primes;
@@ -12,22 +13,27 @@ public final class PrimeFactorizations {
     this.primes = primes;
   }
 
+  /** Decomposes the input into its prime factors. */
   public PrimeFactorization factorizationOf(long n) {
     return PrimeFactorization.of(n, primes);
   }
 
+  /** Generates the proper divisors of the input. */
   public Generator<Long> properDivisorsOf(long n) {
     return properDivisorsOf(factorizationOf(n));
   }
 
+  /** Generates the proper divisors of the input. */
   public Generator<Long> properDivisorsOf(PrimeFactorization number) {
     return new DivisorGenerator(number, true, false);
   }
 
+  /** Returns whether an integer is equal to the sum of its proper divisors. */
   public boolean isPerfect(long n) {
     return n == Generators.from(properDivisorsOf(n)).reduce(0l, (a, b) -> a + b);
   }
 
+  /** Returns whether an integer is less than the sum of its proper divisors. */
   public boolean isAbundant(long n) {
     return n < Generators.from(properDivisorsOf(n)).reduce(0l, (a, b) -> a + b);
   }
---
+++ test/utils/primes/PrimeFactorizationsTest.java
@@ -0,0 +1,54 @@
+package test.utils.primes;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import src.utils.primes.PrimeFactorizations;
+import src.utils.primes.Primes;
+
+public class PrimeFactorizationsTest {
+
+  @Test 
+  public void isPerfect_returnsExpectedResult() {
+    PrimeFactorizations p = new PrimeFactorizations(new Primes());
+
+    assertTrue(p.isPerfect(6)); 
+    assertFalse(p.isPerfect(5));
+    assertFalse(p.isPerfect(7));
+
+    assertTrue(p.isPerfect(28)); 
+    assertFalse(p.isPerfect(27));
+    assertFalse(p.isPerfect(29));
+
+    assertTrue(p.isPerfect(496)); 
+    assertFalse(p.isPerfect(495));
+    assertFalse(p.isPerfect(497));
+
+    assertTrue(p.isPerfect(8128)); 
+    assertFalse(p.isPerfect(8127));
+    assertFalse(p.isPerfect(8129));        
+  }
+
+  @Test 
+  public void isAbundant_returnsExpectedResult() {
+    PrimeFactorizations p = new PrimeFactorizations(new Primes());
+
+    assertTrue(p.isAbundant(12)); 
+    assertFalse(p.isAbundant(11));
+    assertFalse(p.isAbundant(13));
+
+    assertTrue(p.isAbundant(56)); 
+    assertFalse(p.isAbundant(55));
+    assertFalse(p.isAbundant(57));
+
+    assertTrue(p.isAbundant(992)); 
+    assertFalse(p.isAbundant(991));
+    assertFalse(p.isAbundant(993));
+
+    assertTrue(p.isAbundant(945)); 
+    assertFalse(p.isAbundant(944));
+    assertFalse(p.isAbundant(946));        
+  }
+}
