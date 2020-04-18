d0c1b188ef1cafa5787e6069c535d03c90452d03
unknown <nkprasa@gmail.com>
2020-04-18 01:32:37 -0500

    Add unit tests for DivisorGenerator
    Fails some edge cases - added TODO to address

--- test/Assertions.java
+++ test/Assertions.java
@@ -3,7 +3,9 @@ package test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -20,7 +22,7 @@ public class Assertions {
 	public static <T> void assertGenerates(Generator<T> generator, List<T> expected) {
 	  for (T t : expected) {
 			assertTrue(
-          "Generator needs next to match " + t,
+          "Generator does not have next, but expected element " + t,
           generator.hasNext());
 			assertEqual(generator.getNext(), t);
 		}
@@ -28,37 +30,52 @@ public class Assertions {
 	}
 	
 	@SafeVarargs
-    public static <T> void assertGenerates(GeneratorConsumer<T> generatorConsumer, T ... expected) {
-	    assertGenerates(generatorConsumer, Arrays.asList(expected));
+  public static <T> void assertGenerates(GeneratorConsumer<T> generatorConsumer, T ... expected) {
+    assertGenerates(generatorConsumer, Arrays.asList(expected));
 	}
 
 	public static <T> void assertGenerates(GeneratorConsumer<T> generatorConsumer, List<T> expected) {
-	    assertGenerates(generatorConsumer.generator(), expected);
+	  assertGenerates(generatorConsumer.generator(), expected);
 	}
 
 	public static <T> void assertGeneratesNone(Generator<T> generator) {
 		assertFalse(
-			"Generator expected to generate no elements, but hasNext() returns true",
-			generator.hasNext());
+			  "Generator expected to generate no elements, but hasNext() returns true",
+			  generator.hasNext());
 	}
 
 	public static <T> void assertGeneratesNone(GeneratorConsumer<T> generatorConsumer) {
 		assertGeneratesNone(generatorConsumer.generator());
 	}
 
-    @SafeVarargs
-    public static <T> void assertListMatches(List<T> list, T ... expected) {
-	    assertListsMatch(list, Arrays.asList(expected));
+  @SafeVarargs
+  public static <T> void assertListMatches(List<T> list, T ... expected) {
+	  assertListsMatch(list, Arrays.asList(expected));
 	}
 
 	public static <T> void assertListsMatch(List<T> actual, List<T> expected) {
-	    int n = expected.size();
-	    assertEqual(actual.size(), n);
-	
-	    for (int i = 0; i < n; i++) {
-	        assertEqual(actual.get(i), expected.get(i), "Index " + i);
-	    }
+    int n = expected.size();
+    assertEqual(actual.size(), n);
+
+    for (int i = 0; i < n; i++) {
+      assertEqual(actual.get(i), expected.get(i), "Index " + i);
     }
+  }
+
+  public static <T> void assertContainEqualElements(
+      Collection<T> actual, Collection<T> expected) {
+    assertEqual(actual.size(), expected.size());
+    ArrayList<T> actualList = new ArrayList<T>(actual);
+    ArrayList<T> expectedList = new ArrayList<T>(expected);
+    for (T t : actualList) {
+      assertTrue(
+          "Actual contains unexpected element " + t,
+          expectedList.remove(t));
+    }
+    assertTrue(
+        "Actual does not contain expected elements " + expectedList, 
+        expectedList.size() == 0);
+  }
 	
 	public static <K, V> void assertMapsMatch(Map<K, V> expected, Map<K, V> actual) {
 		assertEqual(actual.size(), expected.size(), "Map sizes");
@@ -69,13 +86,13 @@ public class Assertions {
 		}
 	} 
 
-    public static <T> void assertEqual(T actual, T expected) {
-        assertEqual(actual, expected, "");
-    }
+  public static <T> void assertEqual(T actual, T expected) {
+    assertEqual(actual, expected, "");
+  }
 
 	public static <T> void assertEqual(T actual, T expected, String tag) {
-	    assertTrue(
-	        String.format(tag + " actual: %s does not match expected: %s", actual, expected),
-	        actual.equals(expected));
+    assertTrue(
+        String.format(tag + " actual: %s does not match expected: %s", actual, expected),
+        actual.equals(expected));
 	}
 }
---
+++ test/utils/generators/specialized/DivisorGeneratorTest.java
@@ -0,0 +1,133 @@
+package test.utils.generators.specialized;
+
+import static test.Assertions.assertGenerates;
+import static test.Assertions.assertGeneratesNone;
+
+import java.util.Arrays;
+
+import static test.Assertions.assertContainEqualElements;
+
+import org.junit.Test;
+
+import src.utils.generators.Generators;
+import src.utils.generators.consumers.GeneratorConsumer;
+import src.utils.generators.specialized.DivisorGenerator;
+import src.utils.primes.PrimeFactorizations;
+import src.utils.primes.Primes;
+
+public class DivisorGeneratorTest {
+
+  // TODO: Fix failing unit tests in this class
+
+  @Test 
+  public void divisors_ofOne_areOne() {
+    assertGenerates(divisors(1, true, true), 1l);
+  }
+
+  @Test 
+  public void properDivisors_ofOne_areEmpty() {
+    assertGeneratesNone(divisors(1, true, false));
+  }
+
+  @Test 
+  public void nonUnitDivisors_ofOne_areEmpty() {
+    assertGeneratesNone(divisors(1, false, true));
+  }
+
+  @Test 
+  public void properNonUnitDivisors_ofOne_areEmpty() {
+    assertGeneratesNone(divisors(1, false, false));
+  }
+
+  @Test 
+  public void divisors_ofPrime_areOneAndPrime() {
+    assertGenerates(divisors(3, true, true), 1l, 3l);
+  }
+
+  @Test 
+  public void properDivisors_ofPrime_areOne() {
+    assertGenerates(divisors(3, true, false), 1l);
+  }
+
+  @Test 
+  public void nonUnitDivisors_ofPrime_arePrime() {
+    assertGenerates(divisors(3, false, true), 3l);
+  }
+
+  @Test 
+  public void properNonUnitDivisors_ofPrime_areEmpty() {
+    assertGeneratesNone(divisors(3, false, false));
+  }
+
+  @Test 
+  public void divisors_ofPrimeSquare_generatesExpected() {
+    assertContainEqualElements(
+        divisors(25, true, true).list(),
+        Arrays.asList(1l, 5l, 25l));
+  }
+
+  @Test 
+  public void properDivisors_ofPrimeSquare_generatesExpected() {
+    assertContainEqualElements(
+        divisors(25, true, false).list(),
+        Arrays.asList(1l, 5l));
+  }
+
+  @Test 
+  public void nonUnitDivisors_ofPrimeSquare_generatesExpected() {
+    assertContainEqualElements(
+        divisors(25, false, true).list(),
+        Arrays.asList(5l, 25l));
+  }
+
+  @Test 
+  public void properNonUnitDivisors_ofPrimeSquare_generatesExpected() {
+    assertContainEqualElements(
+        divisors(25, false, false).list(),
+        Arrays.asList(5l));
+  }
+
+  @Test 
+  public void divisors_ofComposite_generatesExpected() {
+    assertContainEqualElements(
+        divisors(12, true, true).list(),
+        Arrays.asList(1l, 2l, 3l, 4l, 6l, 12l));
+  }
+
+  @Test 
+  public void properDivisors_ofComposite_generatesExpected() {
+    assertContainEqualElements(
+        divisors(12, true, false).list(),
+        Arrays.asList(1l, 2l, 3l, 4l, 6l));
+  }
+
+  @Test 
+  public void nonUnitDivisors_ofComposite_generatesExpected() {
+    assertContainEqualElements(
+        divisors(12, false, true).list(),
+        Arrays.asList(2l, 3l, 4l, 6l, 12l));
+  }
+
+  @Test 
+  public void properNonUnitDivisors_ofComposite_generatesExpected() {
+    assertContainEqualElements(
+        divisors(12, false, false).list(),
+        Arrays.asList(2l, 3l, 4l, 6l));
+  }
+
+  @Test 
+  public void divisors_ofLargeComposite_generatesExpected() {
+    assertContainEqualElements(
+        divisors(24726, true, true).list(),
+        Arrays.asList(
+            1l, 2l, 3l, 6l, 13l, 26l, 39l, 78l, 317l, 634l,
+            951l, 1902l, 4121l, 8242l, 12363l, 24726l));
+  }
+
+  private static GeneratorConsumer<Long> divisors(
+      long n, boolean includeOne, boolean includeN) {
+    PrimeFactorizations p = new PrimeFactorizations(new Primes());
+    return Generators.from(new DivisorGenerator(p.factorizationOf(n), includeOne, includeN));
+  }
+
+}
