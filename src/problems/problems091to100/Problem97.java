09d4c09bcc3edf952abc23feb181c1f6e8d5bf57
unknown <nkprasa@gmail.com>
2020-04-25 17:04:49 -0500

    Solution to Problem 97
    Co-authored-by: rbarryanderson@gmail.com <rbarryanderson@gmail.com>

---
+++ src/problems/problems091to100/Problem97.java
@@ -0,0 +1,39 @@
+package src.problems.problems091to100;
+
+import static src.utils.numbers.BigNumber.fromLong;
+
+import java.lang.invoke.MethodHandles;
+import src.utils.numbers.BigNumber;
+
+public class Problem97 {
+
+  public static void main(String[] args) {
+    System.out.println(MethodHandles.lookup().lookupClass());
+    long startTime = System.nanoTime();
+    
+    BigNumber prime = 
+        lastDigitsOfPower(fromLong(2), 7830457)
+            .multiplyBy(fromLong(28433l))
+            .addTo(fromLong(1l));
+    System.out.println(lastDigitsOf(prime));
+
+    System.out.println(((System.nanoTime() - startTime) / 1000000) + " ms");
+  }
+
+  private static BigNumber lastDigitsOfPower(BigNumber base, long exponent) {
+    if (exponent == 0) {
+      return fromLong(1l);
+    } else if (exponent % 2 == 0) {
+      BigNumber halfPow = lastDigitsOfPower(base, exponent / 2);
+      BigNumber power = halfPow.multiplyBy(halfPow);
+      return lastDigitsOf(power);
+    } else {
+      BigNumber power = lastDigitsOfPower(base, exponent - 1).multiplyBy(base);
+      return lastDigitsOf(power);
+    }
+  }
+
+  private static BigNumber lastDigitsOf(BigNumber number) {
+     return new BigNumber(number.digits().subList(0, Math.min(10, number.digits().size())));
+  }
+}
