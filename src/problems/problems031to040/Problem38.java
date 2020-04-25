3cb23a72b28c0a21911b16ad51338de7d6c87a54
Automated Code Formatter <>
2020-04-25 18:00:18 +0000

    Automated: Format Code skip-checks: true

--- src/problems/problems031to040/Problem38.java
+++ src/problems/problems031to040/Problem38.java
@@ -13,29 +13,35 @@ public class Problem38 {
     long startTime = System.nanoTime();
 
     range(1, 9999)
-        .map(k -> range(1, 9).reducing("", (str, i) -> str + (k * i)).whileTrue(str -> str.length() <= 9).lastValue().get())
+        .map(
+            k ->
+                range(1, 9)
+                    .reducing("", (str, i) -> str + (k * i))
+                    .whileTrue(str -> str.length() <= 9)
+                    .lastValue()
+                    .get())
         .filter(str -> isPandigital(str))
         .max(str -> Long.parseLong(str))
         .print();
 
     int maxPanDigital = 0;
     for (int i = 1; i < 9999; i++) {
-        String nineDigit = getNineDigit(i);
-        if (isPandigital(nineDigit) && parseInt(nineDigit) > maxPanDigital) {
-            maxPanDigital = parseInt(nineDigit);
-        }
+      String nineDigit = getNineDigit(i);
+      if (isPandigital(nineDigit) && parseInt(nineDigit) > maxPanDigital) {
+        maxPanDigital = parseInt(nineDigit);
+      }
     }
     System.out.println("Max Pan digital: " + maxPanDigital);
-        
+
     System.out.println(((System.nanoTime() - startTime) / 1000000) + " ms");
   }
-  
+
   public static String getNineDigit(Integer input) {
     String value = input.toString();
     Integer curMultiple = 2;
     while (value.length() < 10) {
-        value += input*curMultiple;
-        curMultiple++;
+      value += input * curMultiple;
+      curMultiple++;
     }
     return value;
   }
