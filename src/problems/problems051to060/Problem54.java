1f85be96967cee720588fe13722132ee18c057fe
Automated Code Formatter <>
2020-04-26 18:27:00 +0000

    Automated: Format Code skip-checks: true

--- src/problems/problems051to060/Problem54.java
+++ src/problems/problems051to060/Problem54.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-
 import src.utils.generators.Generators;
 import src.utils.generators.base.tuples.Tuples;
 import src.utils.generators.base.tuples.Tuples.Pair;
@@ -25,47 +24,47 @@ public class Problem54 {
   }
 
   enum Suit {
-      Club,
-      Spade,
-      Heart,
-      Diamond;
+    Club,
+    Spade,
+    Heart,
+    Diamond;
 
     static Suit forValue(Character suit) {
-        if (suit == 'C') {
-          return Club;
-        }
-        if (suit == 'S') {
-          return Spade;
-        }
-        if (suit == 'H') {
-          return Heart;
-        }
-        if (suit == 'D') {
-          return Diamond;
-        }
-        throw new RuntimeException("Invalid value " + suit + " for Suit");
+      if (suit == 'C') {
+        return Club;
+      }
+      if (suit == 'S') {
+        return Spade;
+      }
+      if (suit == 'H') {
+        return Heart;
+      }
+      if (suit == 'D') {
+        return Diamond;
+      }
+      throw new RuntimeException("Invalid value " + suit + " for Suit");
     }
   }
 
   enum CardValue {
-    Two (2),
-    Three (3),
-    Four (4),
-    Five (5),
-    Six (6),
-    Seven (7),
-    Eight (8),
-    Nine (9),
-    Ten (10),
-    Jack (11),
-    Queen (12),
-    King (13),
-    Ace (14);
+    Two(2),
+    Three(3),
+    Four(4),
+    Five(5),
+    Six(6),
+    Seven(7),
+    Eight(8),
+    Nine(9),
+    Ten(10),
+    Jack(11),
+    Queen(12),
+    King(13),
+    Ace(14);
 
     private int cardValue;
 
     private CardValue(int value) {
-        this.cardValue = value;    
+      this.cardValue = value;
     }
 
     int cardValue() {
@@ -73,46 +72,46 @@ public class Problem54 {
     }
 
     public static CardValue forValue(Character value) {
-        if (value == '2') {
-          return Two;
-        }
-        if (value == '3') {
-          return Three;
-        }
-        if (value == '4') {
-          return Four;
-        }
-        if (value == '5') {
-          return Five;
-        }
-        if (value == '6') {
-          return Six;
-        }
-        if (value == '7') {
-          return Seven;
-        }
-        if (value == '8') {
-          return Eight;
-        }
-        if (value == '9') {
-          return Nine;
-        }
-        if (value == 'T') {
+      if (value == '2') {
+        return Two;
+      }
+      if (value == '3') {
+        return Three;
+      }
+      if (value == '4') {
+        return Four;
+      }
+      if (value == '5') {
+        return Five;
+      }
+      if (value == '6') {
+        return Six;
+      }
+      if (value == '7') {
+        return Seven;
+      }
+      if (value == '8') {
+        return Eight;
+      }
+      if (value == '9') {
+        return Nine;
+      }
+      if (value == 'T') {
         return Ten;
-        }
-        if (value == 'J') {
+      }
+      if (value == 'J') {
         return Jack;
-        }
-        if (value == 'Q') {
+      }
+      if (value == 'Q') {
         return Queen;
-        }
-        if (value == 'K') {
+      }
+      if (value == 'K') {
         return King;
-        }
-        if (value == 'A') {
+      }
+      if (value == 'A') {
         return Ace;
-        }
-        throw new RuntimeException("Invalid value " + value + " for CardValue");
+      }
+      throw new RuntimeException("Invalid value " + value + " for CardValue");
     }
   }
 
@@ -131,15 +130,15 @@ public class Problem54 {
   }
 
   enum HandResultType {
-    HighCard (1),
-    OnePair (2),
-    TwoPair (3),
-    ThreeOfAKind (4),
-    Straight (5),
-    Flush (6),
-    FullHouse (7),
-    FourOfAKind (8),
-    StraightFlush (9);
+    HighCard(1),
+    OnePair(2),
+    TwoPair(3),
+    ThreeOfAKind(4),
+    Straight(5),
+    Flush(6),
+    FullHouse(7),
+    FourOfAKind(8),
+    StraightFlush(9);
 
     final int handResultValue;
 
@@ -150,43 +149,43 @@ public class Problem54 {
 
   private static final class Hand {
     private List<Card> cardList;
-    
+
     Hand(List<Card> cards) {
       this.cardList = cards;
     }
 
     void evaluate() {
-        HashMap<CardValue, Integer> occurenceCount = new HashMap<CardValue, Integer>();
-        boolean isStraight = true;
-        CardValue highCard = CardValue.Two;
-        List<Integer> values = new ArrayList<Integer>();
+      HashMap<CardValue, Integer> occurenceCount = new HashMap<CardValue, Integer>();
+      boolean isStraight = true;
+      CardValue highCard = CardValue.Two;
+      List<Integer> values = new ArrayList<Integer>();
 
-        for (Card card : cardList) {
-            values.add(card.cardValue.cardValue());
-            if (occurenceCount.containsKey(card.cardValue)) {
-                occurenceCount.put(card.cardValue, occurenceCount.get(card.cardValue)+1);
-            } else {
-                occurenceCount.put(card.cardValue, Integer.valueOf(1));
-            }
-
-            if (card.cardValue.cardValue() > highCard.cardValue()) {
-                highCard = card.cardValue;
-            }
+      for (Card card : cardList) {
+        values.add(card.cardValue.cardValue());
+        if (occurenceCount.containsKey(card.cardValue)) {
+          occurenceCount.put(card.cardValue, occurenceCount.get(card.cardValue) + 1);
+        } else {
+          occurenceCount.put(card.cardValue, Integer.valueOf(1));
         }
 
-        Collections.sort(values);
-        int previousValue = values.get(0);
-        for (int i = 1; i < values.size(); i++) {
-            int value = values.get(i);
-            if (value - previousValue != 1) {
-                isStraight = false;
-                break;
-            }
-            previousValue = value;
+        if (card.cardValue.cardValue() > highCard.cardValue()) {
+          highCard = card.cardValue;
         }
+      }
 
-        boolean hi = isFlush() && isStraight;
-        System.out.println(hi);
+      Collections.sort(values);
+      int previousValue = values.get(0);
+      for (int i = 1; i < values.size(); i++) {
+        int value = values.get(i);
+        if (value - previousValue != 1) {
+          isStraight = false;
+          break;
+        }
+        previousValue = value;
+      }
+
+      boolean hi = isFlush() && isStraight;
+      System.out.println(hi);
     }
 
     boolean isFlush() {
@@ -209,7 +208,7 @@ public class Problem54 {
 
     private Suit suit;
     private CardValue cardValue;
-    
+
     Card(String s) {
       // TODO TOPHER logic here;
     }
