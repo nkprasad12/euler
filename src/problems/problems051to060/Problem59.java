f742f46bbde319665c0099bc8b63fc81a037be87
nkprasad12 <31296089+nkprasad12@users.noreply.github.com>
2020-04-30 21:19:26 -0700

    Optimize solution to Problem 59 (#18)
    * Optimize solution to Problem 59
    
    - Restrict key to [97, 122], problem says lower case letters only
    - Update incrementKey to return the same list every time.
    
    These together reduce the runtime by half (> 20 s to 12 s)
    
    - Update decrypt to count the number of ' the ' in place
    
    This reduces the runtime from 12 s to under 300 ms.
    
    * Automated: Format Code
    skip-checks: true
    
    Co-authored-by: Automated Code Formatter <>

--- src/problems/problems051to060/Problem59.java
+++ src/problems/problems051to060/Problem59.java
@@ -18,79 +18,64 @@ public class Problem59 {
     System.out.println(((System.nanoTime() - startTime) / 1000000) + " ms");
   }
 
+  private static final int LETTER_MIN = (int) 'a';
+  private static final int LETTER_MAX = (int) 'z';
+  private static final int[] THE_CHARS = {(int) ' ', (int) 't', (int) 'h', (int) 'e', (int) ' '};
+
   public static String solution() {
     List<Integer> bytes = fromTextFile("problem59.txt", ",").map(Integer::parseInt).list();
-    List<Integer> key = new ArrayList<Integer>(Arrays.asList(0, 0, 0));
+    List<Integer> key = new ArrayList<Integer>(Arrays.asList(LETTER_MIN, LETTER_MIN, LETTER_MIN));
 
-    int maxThes = Integer.MIN_VALUE;
+    int maxThes = 0;
     int sum = -1;
     while (key != null) {
-      Pair<Integer, String> result = decrypt(bytes, key);
-      int numThes = countThes(result.second());
+      Pair<Integer, Integer> result = decrypt(bytes, key);
+      int numThes = result.second();
       if (numThes > maxThes) {
-        System.out.println("key: " + key + ", num thes: " + numThes);
         maxThes = numThes;
         sum = result.first();
       }
       key = incrementKey(key);
     }
-
     return String.valueOf(sum);
   }
 
-  private static int countThes(String message) {
-    // TODO: Optimize this
-    String target = " the ";
-    int lastIndex = 0;
-    int count = 0;
-    while (lastIndex != -1) {
-      lastIndex = message.indexOf(target, lastIndex);
-      if (lastIndex != -1) {
-        count++;
-        lastIndex += 5;
-      }
-    }
-    return count;
-  }
-
-  private static Pair<Integer, String> decrypt(List<Integer> bytes, List<Integer> key) {
+  private static Pair<Integer, Integer> decrypt(List<Integer> bytes, List<Integer> key) {
+    int numThes = 0;
+    int patternPosition = 0;
     int sum = 0;
-    char[] message = new char[bytes.size()];
+
     for (int i = 0; i < bytes.size(); i++) {
       int currByte = key.get(i % 3) ^ bytes.get(i);
       sum += currByte;
-      message[i] = (char) currByte;
+      if (currByte == THE_CHARS[patternPosition]) {
+        if (patternPosition == 4) {
+          patternPosition = 0;
+          numThes++;
+        } else {
+          patternPosition++;
+        }
+      } else {
+        patternPosition = 0;
+      }
     }
-    return pair(sum, new String(message));
+    return pair(sum, numThes);
   }
 
   private static List<Integer> incrementKey(List<Integer> key) {
-    Integer value1 = key.get(0);
-    Integer value2 = key.get(1);
-    Integer value3 = key.get(2);
-
-    value3++;
-
-    if (value3 > 127) {
-      value3 = 0;
-      value2++;
+    // We are given that the key is 3 lower case characters.
+    key.set(2, key.get(2) + 1);
+    if (key.get(2) > LETTER_MAX) {
+      key.set(2, LETTER_MIN);
+      key.set(1, key.get(1) + 1);
     }
-
-    if (value2 > 127) {
-      value2 = 0;
-      value1++;
-      System.out.println(key);
+    if (key.get(1) > LETTER_MAX) {
+      key.set(1, LETTER_MIN);
+      key.set(0, key.get(0) + 1);
     }
-
-    if (value1 > 127) {
+    if (key.get(0) > LETTER_MAX) {
       return null;
     }
-
-    List<Integer> result = new ArrayList<Integer>(3);
-    result.add(value1);
-    result.add(value2);
-    result.add(value3);
-
-    return result;
+    return key;
   }
 }
