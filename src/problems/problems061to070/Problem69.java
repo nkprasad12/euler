0cd79c31d3f763ba9c13f033a4cc67926349d693
Automated Code Formatter <>
2020-05-02 19:52:30 +0000

    Automated: Format Code skip-checks: true

--- src/problems/problems061to070/Problem69.java
+++ src/problems/problems061to070/Problem69.java
@@ -1,12 +1,11 @@
 package problems.problems061to070;
 
 import java.lang.invoke.MethodHandles;
-
 import utils.primes.PrimeFactorizations;
 import utils.primes.Primes;
 
 public class Problem69 {
-  
+
   public static void main(String[] args) {
     System.out.println(MethodHandles.lookup().lookupClass());
     long startTime = System.nanoTime();
@@ -15,7 +14,7 @@ public class Problem69 {
   }
 
   public static String solution() {
-    PrimeFactorizations helper = new PrimeFactorizations(new Primes());  
+    PrimeFactorizations helper = new PrimeFactorizations(new Primes());
     double phiRatio = 0;
     long maxN = 0;
     for (long n = 1; n <= 1000000; n++) {
