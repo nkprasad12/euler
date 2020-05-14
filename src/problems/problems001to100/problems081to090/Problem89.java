d4d41df3f460b66a669b23f7e92a4eb13bf8ca45
ReidAnderson <rbarryanderson@gmail.com>
2020-05-14 16:41:43 -0500

    Solution for Problem 89 (#43)
    * Solution for Problem 89
    
    * Differences in Java version?
    
    * Remove offending import
    
    * Automated: Format Code
    skip-checks: true
    
    Co-authored-by: Automated Code Formatter <>

--- src/problems/problems001to100/problems081to090/Problem89.java
+++ src/problems/problems001to100/problems081to090/Problem89.java
@@ -1,9 +1,31 @@
 package problems.problems001to100.problems081to090;
 
 import java.lang.invoke.MethodHandles;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import utils.generators.Generators;
 
 public class Problem89 {
+
+  static Map<String, Integer> numeralMap =
+      new HashMap<String, Integer>() {
+        {
+          put("I", 1);
+          put("V", 5);
+          put("X", 10);
+          put("L", 50);
+          put("C", 100);
+          put("D", 500);
+          put("M", 1000);
+          put("IV", 4);
+          put("IX", 9);
+          put("XL", 40);
+          put("XC", 90);
+          put("CD", 400);
+          put("CM", 900);
+        }
+      };
 
   public static void main(String[] args) {
     System.out.println(MethodHandles.lookup().lookupClass());
@@ -13,35 +35,132 @@ public class Problem89 {
   }
 
   public static String solution() {
-    return Generators.fromTextFile("problem89.txt")
-        .map(roman -> roman.length() - reduce(roman).length())
-        .reduce(0, (sum, saved) -> sum + saved)
-        .toString();
+    String result =
+        Generators.fromTextFile("problem89.txt")
+            .map(
+                roman ->
+                    roman.length()
+                        - getRomanNumeralFromNumber(getNumberFromRomanNumeral(roman)).length())
+            .reduce(0, (sum, saved) -> sum + saved)
+            .toString();
+
+    return result;
+  }
+
+  public static String getRomanNumeralFromNumber(int n) {
+    LinkedHashMap<String, Integer> numeralCount = new LinkedHashMap<String, Integer>();
+
+    int numeralAmount = 0;
+
+    numeralAmount = n / 1000;
+    numeralCount.put("M", numeralAmount);
+    n -= numeralAmount * 1000;
+
+    numeralAmount = n / 500;
+    numeralCount.put("D", numeralAmount);
+    n -= numeralAmount * 500;
+
+    numeralAmount = n / 100;
+    numeralCount.put("C", numeralAmount);
+    n -= numeralAmount * 100;
+
+    CollapseVals(numeralCount, "C", 4, "D", 1, "CM");
+    CollapseVals(numeralCount, "C", 4, null, null, "CD");
+
+    numeralAmount = n / 50;
+    numeralCount.put("L", numeralAmount);
+    n -= numeralAmount * 50;
+
+    numeralAmount = n / 10;
+    numeralCount.put("X", numeralAmount);
+    n -= numeralAmount * 10;
+
+    CollapseVals(numeralCount, "X", 4, "L", 1, "XC");
+    CollapseVals(numeralCount, "X", 4, null, null, "XL");
+
+    numeralAmount = n / 5;
+    numeralCount.put("V", numeralAmount);
+    n -= numeralAmount * 5;
+
+    numeralAmount = n;
+    numeralCount.put("I", numeralAmount);
+    n -= numeralAmount;
+
+    CollapseVals(numeralCount, "I", 4, "V", 1, "IX");
+    CollapseVals(numeralCount, "I", 4, null, null, "IV");
+
+    String strResult = "";
+    for (Map.Entry<String, Integer> val : numeralCount.entrySet()) {
+      for (int i = 0; i < val.getValue(); i++) {
+        strResult += val.getKey();
+      }
+    }
+
+    return strResult;
+  }
+
+  public static void CollapseVals(
+      LinkedHashMap<String, Integer> values,
+      String val1,
+      Integer count1,
+      String val2,
+      Integer count2,
+      String newValue) {
+    if (val2 == null) {
+      if (values.get(val1) == count1) {
+        values.put(newValue, 1);
+        values.put(val1, 0);
+      }
+    } else {
+      if (values.get(val1) == count1 && values.get(val2) == count2) {
+        values.put(newValue, 1);
+        values.put(val1, 0);
+        values.put(val2, 0);
+      }
+    }
+  }
+
+  public static int getNumberFromRomanNumeral(String numeral) {
+    int number = 0;
+
+    char[] numerals = numeral.toCharArray();
+
+    int i = 0;
+    while (i < numerals.length) {
+      char c1 = numerals[i];
+      char c2 = '?';
+      if (i != numerals.length - 1) {
+        c2 = numerals[i + 1];
+      }
+
+      if (valueOfPair(c1, c2) != null) {
+        number += valueOfPair(c1, c2);
+        i++;
+      } else {
+        number += value(c1);
+      }
+
+      i++;
+    }
+
+    return number;
+  }
+
+  static Integer valueOfPair(char c1, char c2) {
+    Integer value = numeralMap.getOrDefault("" + c1 + c2, null);
+
+    return value;
   }
 
   static int value(char c) {
-    if (c == 'I') {
-      return 1;
+
+    Integer value = numeralMap.getOrDefault("" + c, null);
+
+    if (value == null) {
+      throw new RuntimeException("Invalid Roman numeral");
     }
-    if (c == 'V') {
-      return 5;
-    }
-    if (c == 'X') {
-      return 10;
-    }
-    if (c == 'L') {
-      return 50;
-    }
-    if (c == 'C') {
-      return 100;
-    }
-    if (c == 'D') {
-      return 500;
-    }
-    if (c == 'M') {
-      return 1000;
-    }
-    throw new RuntimeException("Invalid Roman numeral");
+
+    return value;
   }
 
   static String reduce(String num) {
