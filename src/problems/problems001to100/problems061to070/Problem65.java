690699134c673ba201fcfec313bdde8243fcd4c7
Automated Code Formatter <>
2020-05-15 13:28:40 +0000

    Automated: Format Code skip-checks: true

--- src/problems/problems001to100/problems061to070/Problem65.java
+++ src/problems/problems001to100/problems061to070/Problem65.java
@@ -3,9 +3,8 @@ package problems.problems001to100.problems061to070;
 import static utils.generators.Generators.from;
 import static utils.numbers.BigNumber.fromLong;
 
-import utils.numbers.BigNumber;
-
 import java.lang.invoke.MethodHandles;
+import utils.numbers.BigNumber;
 
 public class Problem65 {
 
@@ -20,7 +19,7 @@ public class Problem65 {
     BigNumber num = fromLong(1); // r_100 = 1
     BigNumber den = fromLong(1);
     for (int i = 99; i >= 1; i--) {
-      // [2; 1, 2, 1, 1, 4, 1, 1, 6, ....] 
+      // [2; 1, 2, 1, 1, 4, 1, 1, 6, ....]
       long r = 1;
       if (i == 1) {
         r = 2;
@@ -31,39 +30,38 @@ public class Problem65 {
       BigNumber tmp = num.multiplyBy(fromLong(r)).addTo(den);
       den = num;
       num = tmp;
-    } 
+    }
     return from(num.digits()).reduce(0, (sum, digit) -> sum + digit).toString();
   }
 }
 
-
 // [r1; r2, r3, r4]
-//        1 
+//        1
 // r1 + -------
 //      r2 + 1
 //           ---------
 //           r3 + 1
 //                ----------
 //                r4 + 1
-// = 
-//        1 
+// =
+//        1
 // r1 + -------
 //      r2 + 1
 //           ---------
 //           r3 + 1
 //                ----------
 //                p0 / q0
-// = 
-//        1 
+// =
+//        1
 // r1 + -------
 //      r2 + 1
 //           ---------
 //           (p0 * r3 + q) / p0
-// = 
-//        1 
+// =
+//        1
 // r1 + -------
 //      r2 + 1
 //           ---------
 //           p1 / q1
 // r + 1 / (p / q)
-// 
+//
