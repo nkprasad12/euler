d45c8a358bfad38aa4a6f5503cde6c01266b4580
Automated Code Formatter <>
2020-08-02 02:01:43 +0000

    Automated: Format Code skip-checks: true

--- src/problems/problems101to200/problems101to110/Problem102.java
+++ src/problems/problems101to200/problems101to110/Problem102.java
@@ -3,7 +3,6 @@ package problems.problems101to200.problems101to110;
 import static java.lang.Integer.valueOf;
 
 import java.lang.invoke.MethodHandles;
-
 import utils.generators.Generators;
 import utils.generators.base.tuples.Tuples;
 import utils.generators.base.tuples.Tuples.Pair;
@@ -22,8 +21,11 @@ public class Problem102 {
     // X(-175,41), Y(-421,-714), Z(574,-645)
     // System.out.println(containsOrigin("-340,495,-153,-910,835,-947"));
     // System.out.println(containsOrigin("-175,41,-421,-714,574,-645"));
-    return Generators.fromTextFile("problem102.txt", "\n").filter(points -> containsOrigin(points))
-       .reducing(0, (ct, next) -> ct + 1).lastValue().toString();
+    return Generators.fromTextFile("problem102.txt", "\n")
+        .filter(points -> containsOrigin(points))
+        .reducing(0, (ct, next) -> ct + 1)
+        .lastValue()
+        .toString();
 
     // System.out.println(getIntercepts(Tuples.pair(-153, -910), Tuples.pair(835, -947)));
     // System.out.println(getIntercepts(Tuples.pair(-340, 495), Tuples.pair(-153, -910)));
@@ -37,7 +39,7 @@ public class Problem102 {
     Pair<Integer, Integer> a = Tuples.pair(valueOf(points[0]), valueOf(points[1]));
     Pair<Integer, Integer> b = Tuples.pair(valueOf(points[2]), valueOf(points[3]));
     Pair<Integer, Integer> c = Tuples.pair(valueOf(points[4]), valueOf(points[5]));
-    
+
     boolean hasPositiveXIntercept = false;
     boolean hasNegativeXIntercept = false;
     boolean hasPositiveYIntercept = false;
@@ -103,11 +105,15 @@ public class Problem102 {
         hasNegativeYIntercept = true;
       }
     }
-    
-    return hasNegativeXIntercept && hasPositiveXIntercept && hasNegativeYIntercept && hasPositiveYIntercept;
+
+    return hasNegativeXIntercept
+        && hasPositiveXIntercept
+        && hasNegativeYIntercept
+        && hasPositiveYIntercept;
   }
 
-  static Pair<Double, Double> getIntercepts(Pair<Integer, Integer> point1, Pair<Integer, Integer> point2) {
+  static Pair<Double, Double> getIntercepts(
+      Pair<Integer, Integer> point1, Pair<Integer, Integer> point2) {
     double slope = 0;
     double b = 0;
 
@@ -115,24 +121,24 @@ public class Problem102 {
     Double xIntercept = null;
 
     if (point1.first() - point2.first() == 0) {
-        return Tuples.pair(1.0*point1.first(), yIntercept);
+      return Tuples.pair(1.0 * point1.first(), yIntercept);
     }
 
     slope = (1.0 * (point1.second() - point2.second())) / (point1.first() - point2.first());
-    b = point1.second() - slope*point1.first();
+    b = point1.second() - slope * point1.first();
 
-    if (((b <= point2.second() && b >= point1.second()) || (b >= point2.second() && b <= point1.second()))) {
-        yIntercept = b;
-    }
-    
-    double possibleXIntercept = -1*b / slope;
-
-    if ((possibleXIntercept >= point2.first() && possibleXIntercept <= point1.first()) || possibleXIntercept <=point2.first() && possibleXIntercept >= point1.first()) {
-        xIntercept = possibleXIntercept;
+    if (((b <= point2.second() && b >= point1.second())
+        || (b >= point2.second() && b <= point1.second()))) {
+      yIntercept = b;
     }
 
-    return Tuples.pair(xIntercept, yIntercept);    
+    double possibleXIntercept = -1 * b / slope;
+
+    if ((possibleXIntercept >= point2.first() && possibleXIntercept <= point1.first())
+        || possibleXIntercept <= point2.first() && possibleXIntercept >= point1.first()) {
+      xIntercept = possibleXIntercept;
+    }
+
+    return Tuples.pair(xIntercept, yIntercept);
   }
 }
-
-
